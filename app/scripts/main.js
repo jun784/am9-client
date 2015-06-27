3a309c7743210c709ca5bd941d153eb93e18dfa3
katashin <ktsn55@gmail.com>
2015-06-27 16:31:25 +0900

    use vue.js and page.js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,23 @@
-console.log('\'Allo \'Allo!');
+'use strict';
+
+var app = new Vue({
+  el: "#container",
+  data: {
+    main: undefined
+  }
+});
+Vue.component("home", {
+  template: "#home"
+});
+Vue.component("example", {
+  template: "#example"
+});
+
+page('/', function(ctx) {
+  app.main = "home";
+});
+page('/tmp/example/:q', function(ctx) {
+  app.main = "example";
+  // ctx.paramsをどうやってexampleコンポーネントに渡す？
+});
+page();
