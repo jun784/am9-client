fc308d768b6d72319e4d1d0a0aecd353517b468c
katashin <ktsn55@gmail.com>
2015-06-28 12:39:20 +0900

    facebook login

---
+++ app/scripts/auth.js
@@ -0,0 +1,47 @@
+function getUrlVars () {
+  var vars = {}
+  var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&')
+  for (var i = 0; i < hashes.length; i++) {
+    var hash = hashes[i].split('=')
+    vars[hash[0]] = hash[1]
+  }
+  return vars
+}
+
+var code = getUrlVars().code
+
+if (code) {
+  $.ajax({
+    method: 'POST',
+    url: '/auth/facebook',
+    data: {
+      clientId: '966482230069701',
+      code: code,
+      redirectUri: (window.location.origin || window.location.protocol + '//' + window.location.host) + '/'
+    },
+    dataType: 'json',
+    success: function (res) {
+      console.log(res)
+      localStorage.setItem('token', res.token)
+      $.ajax({
+        url:'/user',
+        method:'GET',
+        success: function (data) [
+          alert('Hello, ' + data.username + '!!')
+        ]
+      })
+    }
+  })
+}
+
+var auth = {}
+auth.login = function () {
+  window.location = 'https://www.facebook.com/dialog/oauth?%20client_id=966482230069701%20&redirect_uri=' + (window.location.origin || window.location.protocol + '//' + window.location.host) + '/'
+}
+auth.check = function () {
+  return !!localStorage.getItem('token')
+}
+
+auth.logout = function () {
+  localStorage.removeItem('token')
+}
