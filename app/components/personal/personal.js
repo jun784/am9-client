ea4c274ace39a32e9fe72e04ec44131b46dde2b2
katashin <ktsn55@gmail.com>
2015-07-26 23:24:09 +0900

    get the user's resource with a http request.

--- app/components/personal/personal.js
+++ app/components/personal/personal.js
@@ -1,3 +1,5 @@
+'use strict';
+
 import timeline from '../timeline/timeline';
 import add from '../add/add';
 import things from '../things/things';
@@ -6,10 +8,21 @@ require('./personal.scss');
 
 module.exports = {
   template: require('./personal.html'),
+  data: function() {
+    return {
+      resources: [],
+      things: []
+    };
+  },
 
   created: function() {
     this.$on('doing-added', (idx) => {
       this.$broadcast('doing-added', idx);
+    });
+
+    var resource = this.$resource('/api/v1/resources/:id');
+    resource.get({ id: 1, date: this.todayStr() }, (resource) => {
+      this.resources = [resource];
     });
 
     this.things = [{
@@ -31,50 +44,16 @@ module.exports = {
       time: 1000 * 60 * 55,
       done: false
     }];
+  },
 
-    // dummy data
-    this.resources = [
-      {
-        id: 1,
-        doings: [{
-          id: 1,
-          body: 'シャワーを浴びる',
-          start: new Date(2015, 5, 28, 8, 0).getTime(),
-          time: 1000 * 60 * 60 * 2,
-          done: false
-        }, {
-          id: 2,
-          body: 'ご飯を食べる',
-          start: new Date(2015, 5, 28, 10, 30).getTime(),
-          time: 1000 * 60 * 25,
-          done: false
-        }, {
-          id: 3,
-          body: '掃除をする',
-          start: new Date(2015, 5, 28, 11, 0).getTime(),
-          time: 1000 * 60 * 25,
-          done: false
-        }, {
-          id: 11,
-          body: 'お風呂に入る',
-          start: new Date(2015, 5, 28, 13, 0).getTime(),
-          time: 1000 * 60 * 115,
-          done: false
-        }, {
-          id: 12,
-          body: 'カラオケ',
-          start: new Date(2015, 5, 28, 15, 0).getTime(),
-          time: 1000 * 60 * 55,
-          done: false
-        }, {
-          id: 13,
-          body: '◯◯の開発',
-          start: new Date(2015, 5, 28, 17, 0).getTime(),
-          time: 1000 * 60 * 175,
-          done: false
-        }]
-      }
-    ];
+  methods: {
+    todayStr: function() {
+      var today = new Date();
+      var year = today.getFullYear();
+      var month = ('0' + (today.getMonth() + 1)).slice(-2);
+      var date = ('0' + today.getDate()).slice(-2);
+      return `${year}-${month}-${date}`;
+    }
   },
 
   components: { timeline, add, things }
